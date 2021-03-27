8a329e69e0bd517ed709852cc067cd1eadbdea82
devVenus1202 <venus1202winter@gmail.com>
2021-03-27 18:44:35 +0000

    add style

--- src/components/Repository.tsx
+++ src/components/Repository.tsx
@@ -5,5 +5,26 @@ export interface IProps {
 }
 export default function Repository(props: IProps) {
   const { repository } = props;
-  return <div>{repository.name}</div>;
+  return (
+    <div className="repository">
+      <div className="repository-name">
+        <div>
+          {repository.name}(
+          <a href={repository.url} target="_blank">
+            {repository.url}
+          </a>
+          )
+        </div>
+        <div className="description">{repository.description}</div>
+      </div>
+      <div className="repository-meta">
+        <div>
+          <b>Owner:</b> {repository.owner.login}
+        </div>
+        <div>
+          <b>Stars:</b> {repository.stargazers.totalCount}
+        </div>
+      </div>
+    </div>
+  );
 }
